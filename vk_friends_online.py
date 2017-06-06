10bd482ba78ab26dd43cd79b974c42009ad78f25
Paul Vergeev <paulvergeev@gmail.com>
2017-06-06 17:05:50 +0300

    Solve the problem

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,45 @@
+import os
+from getpass import getpass
+
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
-
-def get_user_login():
-    pass
-
-
-def get_user_password():
-    pass
-
-
-def get_online_friends(login, password):
+def get_online_friends(login, password, app_id):
+    # Implementation notes:
+    #   We will use friends.get instead of friends.getOnline.
+    # The former is inefficient and error-prone:
+    # https://gist.github.com/patrnk/eba5ab7bf1704253cc01aa78bcb07005
+    #   One friends.get request can only return 5000 friends,
+    # whereas a user can have up to 10000 friends online. That's why we make a second request
+    # (see https://vk.com/dev/friends.get).
+    fields = ['first_name', 'last_name'] 
+    friend_count_limit = 5000
     session = vk.AuthSession(
-        app_id=APP_ID,
+        app_id=app_id,
         user_login=login,
         user_password=password,
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends = api.friends.get(fields=fields, count=friend_count_limit)
+    if len(friends) == friend_count_limit:
+        friends += api.friends.get(fields=fields, offset=friend_count_limit)
+    return [friend for friend in friends if friend['online']]
 
 
-def output_friends_to_console(friends_online):
-    pass
+def print_friend(friend):
+    output_string_template = '* {first_name} {last_name}'
+    output_string = output_string_template.format(
+        first_name=friend['first_name'],
+        last_name=friend['last_name'],
+    )
+    print(output_string)
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    app_id = os.environ['VK_APP_ID']
+    login = input('Login: ')
+    password = getpass('Password: ')
+    friends_online = get_online_friends(login, password, app_id)
+    print("Here's your online friends:")
+    for online_friend in friends_online:
+        print_friend(online_friend)
